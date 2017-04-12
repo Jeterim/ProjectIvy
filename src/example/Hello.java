51ed32723b721d25895e2bf39b12d59d46b3cfb5
jeterim <jeterim2@gmail.com>
2017-04-12 21:47:12 +0200

    Ajout fin partie 2 + documentation

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -11,24 +11,32 @@ import java.io.IOException;
 import com.opencsv.*;
 
 public class Hello {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException {
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
-
+        
         int monmax = 0;
-        CSVReader reader = new CSVReader(new FileReader("data.csv")); 
-        List<String[]> myEntries = reader.readAll();
-        for(String[] entry : myEntries) {
-            int nbread = Integer.parseInt(Arrays.toString(entry).substring(1, Arrays.toString(entry).length()-1));
-            if(nbread > monmax){
-                monmax = nbread;
-            }
-            System.out.println("Nombre lu : " + nbread);
+        try {
+            
+            CSVReader reader = new CSVReader(new FileReader("data.csv")); 
+            List<String[]> myEntries = reader.readAll();
+            for(String[] entry : myEntries) {
+                for (int i = 0; i < entry.length; i++) {
+                    int nbread = Integer.parseInt(entry[i]);
+                    if(nbread > monmax){
+                        monmax = nbread;
+                    }
+                    System.out.println("Nombre lu : " + nbread);
+                }
 
+            }
+            System.out.println("Nombre max : " + monmax);
         }
-        System.out.println("Nombre max : " + monmax);
+        catch(IOException e){
+            System.out.println("Probleme de lecture du fichier csv" + e);
+        }
 
     }
 }
